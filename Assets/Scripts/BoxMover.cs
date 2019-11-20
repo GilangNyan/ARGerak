eef40f630b1f05fac09bf71ac28ce1f13dd871ae
Gilang Saeful Anwar <sagilang@gmail.com>
2019-11-20 20:03:24 +0700

    Initial commit

---
+++ Assets/Scripts/BoxMover.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxMover : MonoBehaviour
+{
+    public Joystick joystick;
+    public float speed = 10f;
+    public Transform box;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float rotX = joystick.Horizontal * speed;
+        float rotY = joystick.Vertical * speed;
+
+        transform.Rotate(rotX, rotY, 0);
+    }
+}
